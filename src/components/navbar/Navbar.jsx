2e7cf67bd8d02881eb16f5dc52f69045d88d141d
sanathsb98 <itssanathsb@gmail.com>
2024-01-24 15:54:02 +0530

    commits

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,7 +7,6 @@ import { useInView } from 'react-intersection-observer';
 const Navbar = () => {
 
     const[isNavbarToggled,setIsNavbarToggled] = useState(false)
-
     const navbarspace = useRef(null)
     const navbarcontent = useRef(null)
     const navbarleft = useRef(null)
@@ -18,7 +17,6 @@ const Navbar = () => {
     const shimmeringlights = useRef(null)
     const crossicon = useRef(null)
     
-
     useEffect(() => {
 
         if (isNavbarToggled) {
@@ -60,10 +58,9 @@ const Navbar = () => {
             navbarcontent.current.style.alignItems = 'center';
             navbarleft.current.style.padding= '0px'; 
             crossicon.current.style.marginTop ='0px'
-        
-          
         }
 
+        
     }, [isNavbarToggled])
 
     const animation = {
@@ -76,21 +73,18 @@ const Navbar = () => {
             <div  ref={navbarcontent}  className='navbar-content'>
                 <div ref={navbarleft} className='navbar-left'>
                     <img ref={shimmeringlights} className='shimmering-logo' src={logo} width='100%' height='100%' />
-                    <motion.div ref={mobiletab1} whileHover={{scale : 1.2}} className='navbar-mobile-tab'>abouts</motion.div>
+                    <motion.div ref={mobiletab1} whileHover={{scale : 1.2}} className='navbar-mobile-tab'>about</motion.div>
                     <motion.div  ref={mobiletab2} whileHover={{scale : 1.2}} className='navbar-mobile-tab'>services</motion.div>
                     <motion.div  ref={mobiletab3}  whileHover={{scale : 1.2}} className='navbar-mobile-tab'>product</motion.div>
                     <motion.div  ref={mobiletab4}  whileHover={{scale : 1.2}} className='navbar-mobile-tab'>contact</motion.div>
                 </div>
                 <div className='navbar-right'>
-                    <motion.div whileHover={{scale : 1.2}} className='navbar-tab'>abouts</motion.div>
+                    <motion.div whileHover={{scale : 1.2}} className='navbar-tab'>about</motion.div>
                     <motion.div whileHover={{scale : 1.2}} className='navbar-tab'>services</motion.div>
                     <motion.div  whileHover={{scale : 1.2}} className='navbar-tab'>product</motion.div>
                     <motion.div  whileHover={{scale : 1.2}} className='navbar-tab'>contact</motion.div>
                     <div onClick={()=>setIsNavbarToggled(!isNavbarToggled)} ref={crossicon}  className='burger-menu'>
                       {isNavbarToggled ? (<svg  width="50px" height="50px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="white"><path d="M6.75827 17.2426L12.0009 12M17.2435 6.75736L12.0009 12M12.0009 12L6.75827 6.75736M12.0009 12L17.2435 17.2426" stroke="#FFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>):
-                      
-                      
-                      
                       
                       (<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 40 40" fill="none">
                       <path d="M6.25 20H33.75" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
