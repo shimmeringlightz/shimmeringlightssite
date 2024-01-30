ed94865bf9324f2c8f4e075196912b19069eb183
sanathsb98 <itssanathsb@gmail.com>
2024-01-30 15:48:25 +0530

    commit

--- src/components/contactmodal/ContactModale.jsx
+++ src/components/contactmodal/ContactModale.jsx
@@ -1,26 +1,35 @@
 import React, { useEffect, useState } from 'react';
+import emailjs from 'emailjs-com';
 import '../contactmodal/ContactModale.css';
-import {motion} from 'framer-motion'
 
 const ContactModale = () => {
 
-    const [formState,setFormState] = useState({})
+    const [formState, setFormState] = useState({})
 
     const closeModale = () => {
-     
-            document.getElementById("modalecontainer").style.display = 'none';
-       
+        document.getElementById("modalecontainer").style.display = 'none';
     }
 
     const changeHandler = (event) => {
-        setFormState({...formState , [event.target.name]:event.target.value})
-        
+        setFormState({ ...formState, [event.target.name]: event.target.value })
     }
-    console.log(formState)
+
+    const sendEmail = () => {
+        emailjs.send('service_jl2tinc', 'template_0scbd9l', formState, '0fyyCkU6Ci1GObofE')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+            closeModale()
+    };
+
+    // console.log(formState)
+
     return (
         <div id='modalecontainer' className='contact-modale-container'  >
             <div className='contact-modale-content'>
-                <div onClick={()=>closeModale()} className='close-contact-modale'>X</div>
+                <div onClick={() => closeModale()} className='close-contact-modale'>X</div>
                 <div className='contact-msg-title'>Contact Us</div>
                 <div className='contact-input-wrapper'>
                     <div className='contact-input-subtext'>Name</div>
@@ -28,16 +37,15 @@ const ContactModale = () => {
                 </div>
                 <div className='contact-input-wrapper'>
                     <div className='contact-input-subtext'>Email</div>
-                    <input placeholder='Email' type='email' name='email' onChange={changeHandler}  className='contact-msg-email' />
+                    <input placeholder='Email' type='email' name='email' onChange={changeHandler} className='contact-msg-email' />
                 </div>
                 <div className='contact-input-wrapper'>
                     <div className='contact-input-subtext'>Message</div>
-                    <textarea placeholder='Message' rows="6" name='message' onChange={changeHandler}  className='contact-msg-detail' />
+                    <textarea placeholder='Message' rows="6" name='message' onChange={changeHandler} className='contact-msg-detail' />
                 </div>
-                <div  onClick={()=>closeModale()} className='contact-msg-btn'>Submit</div>
+                <div onClick={() => sendEmail()} className='contact-msg-btn'>Submit</div>
             </div>
         </div>
-
     )
 }
 
